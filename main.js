6eaa9a6986219bdf7e9d2d16fcb89568456cc393
Haraneesh Shah <haraneesh.shah8@gmail.com>
2025-02-24 22:17:04 +1100

    Updated to google

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const { chromium } = require('playwright');
 
-{async () => {
+(async () => {
     const browser = await chromium.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.apple.com');
+    await page.goto('https://www.google.com');
     const bodyHandle = await page.$('body');
     const html = await page.evaluate(body => body.innerText, bodyHandle);
     console.log(html);
     await browser.close();
-}}
+})();
