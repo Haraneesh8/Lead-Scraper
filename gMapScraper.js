daea5eb6b265a75dffc33513ebbffbf146dfde1b
Haraneesh Shah <haraneesh.shah8@gmail.com>
2025-03-01 18:01:43 +1100

    Combined individual scraper and gMapScraper to create a CSV file with all information in their respective rows

--- gMapScraper.js
+++ gMapScraper.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 
 (async () => {
     
+    csvFileName = 'realEstate.csv';
     googleMapUrl = 'https://www.google.com/maps/search/real+estate/@-33.8662563,151.1723894,15z/data=!3m1!4b1?entry=ttu&g_ep=EgoyMDI1MDIyMy4xIKXMDSoASAFQAw%3D%3D'
 
     // Open up the browser on a newpage and go to the google map url
@@ -36,6 +37,7 @@ const fs = require('fs');
         if (placeCount === prevCount){
             endOfList = true;
             console.log('End Of List Reached');
+            break;
         } else {
             prevCount = placeCount;
         }
@@ -46,8 +48,51 @@ const fs = require('fs');
     // Extract the url's of each of the places
     const urls = await page.$$eval('a', links => links.map(link => link.href).filter(href => href.startsWith('https://www.google.com/maps/place/')));
 
-    //write to CSV file
-    fs.writeFileSync('gMapUrls.csv', urls.join('\n'));
+    const individualPageData = async (url) => {
+        const individualPage = await browser.newPage();
+        await individualPage.goto(url);
+        await individualPage.waitForSelector('[jstcache="3"]');
+
+        const name = await individualPage.$('xpath=/html/body/div[2]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[1]/h1');
+        let nameText = name ? await individualPage.evaluate(element => element.textContent, name) : '';
+        nameText = `"${nameText}"`;
+
+        const rating = await individualPage.$('xpath=/html/body/div[2]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div[1]/div[2]/span[1]/span[1]');
+        let ratingText = rating ? await individualPage.evaluate(element => element.textContent, rating) : '';
+        ratingText = `"${ratingText}"`;
+
+        const category = await individualPage.$('xpath=/html/body/div[2]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div[2]/span[1]/span/button');
+        let categoryText = category ? await individualPage.evaluate(element => element.textContent, category) : '';
+        categoryText = `"${categoryText}"`;
+
+        const website = await individualPage.$('a[data-tooltip="Open website"]') || await individualPage.$('a[data-tooltip="Open menu link"]');
+        let websiteText = website ? await individualPage.evaluate(element => element.getAttribute('href'), website) : '';
+        websiteText = `"${websiteText}"`;
+
+        const phone = await individualPage.$('button[data-tooltip="Copy phone number"]');
+        let phoneText = phone ? await individualPage.evaluate(element => element.textContent, phone) : '';
+        phoneText = `"${phoneText}"`;
+
+        url = `"${url}"`;
+        await individualPage.close();
+        return { nameText, ratingText, categoryText, websiteText, phoneText, url };
+    };
+
+    // Batch processing
+    const batchSize = 5; // Adjust batch size based on your system capability
+    const results = [];
+
+    for (let i = 0; i < urls.length; i += batchSize) {
+        const batchUrls = urls.slice(i, i + batchSize);
+        const batchResults = await Promise.all(batchUrls.map(url => individualPageData(url)));
+        results.push(...batchResults);
+        console.log(`Batch ${i / batchSize + 1} completed.`);
+    }
+
+    // Convert results to CSV format and write to file
+    const csvHeader = 'Name,Rating,Category,Website,Phone,Url\n';
+    const csvRows = results.map(r => `${r.nameText},${r.ratingText},${r.categoryText},${r.websiteText},${r.phoneText},${r.url}`).join('\n');
+    fs.writeFileSync(csvFileName, csvHeader + csvRows);
 
     await browser.close();
 })();
--- urlScraper.js
+++ urlScraper.js
@@ -1,20 +1,43 @@
-const { chromium} = require('playwright');
-
-(async () => {
-
-    individualUrl = 'https://www.google.com/maps/place/Balmain+Realty/@-33.860419,151.168171,16z/data=!3m1!4b1!4m6!3m5!1s0x6b12afb74f36e251:0x8f174001cb4a6fa6!8m2!3d-33.860419!4d151.168171!16s%2Fg%2F1tfvhc8m?authuser=0&hl=en&entry=ttu&g_ep=EgoyMDI1MDIyNi4xIKXMDSoASAFQAw%3D%3D';
+//Initial draft of individual page scrapper which was then combined with the gMap Scraper
 
 
-    const browser = await chromium.launch();
-    const context = await browser.newContext({headless: false});
-    const individualPage = await context.newPage();
-    await individualPage.goto(individualUrl);
 
-    await page.waitForSelector("[jstcache = '3']");
+// const { chromium } = require('playwright');
+// const fs = require('fs');
 
-    const name = await individualPage.$$('/html/body/div[2]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[1]/h1');
-    console.log(name);
+// (async () => {
+
+//     individualUrl = 'https://www.google.com/maps/place/McGrath+Estate+Agents+Leichhardt/@-33.8775524,151.1534392,17z/data=!3m2!4b1!5s0x6b12b01db994d783:0xf1b984aa6612ca0d!4m6!3m5!1s0x6b12b01db842c077:0xe21678b236446064!8m2!3d-33.8775524!4d151.1560141!16s%2Fg%2F1thz1tts?authuser=0&hl=en&entry=ttu&g_ep=EgoyMDI1MDIyNi4xIKXMDSoASAFQAw%3D%3D';
 
 
-    await browser.close();
-});
+//     const browser = await chromium.launch();
+//     const context = await browser.newContext();
+//     const individualPage = await context.newPage();
+    
+//     await individualPage.goto(individualUrl);
+
+//     await individualPage.waitForSelector("[jstcache = '3']");
+
+//     const name = await individualPage.$('xpath=/html/body/div[2]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[1]/h1');
+//     let nameText = name ? await individualPage.evaluate(element => element.textContent, name) : '';
+//     console.log(nameText);
+
+//     const rating = await individualPage.$('xpath=/html/body/div[2]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div[1]/div[2]/span[1]/span[1]');
+//     let ratingText = rating ? await individualPage.evaluate(element => element.textContent, rating) : '';
+//     console.log(ratingText);
+
+//     const category = await individualPage.$('xpath=/html/body/div[2]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div[2]/span[1]/span/button');
+//     let categoryText = category ? await individualPage.evaluate(element => element.textContent, category) : '';
+//     console.log(categoryText);
+
+//     const website = await individualPage.$('a[data-tooltip="Open website"]') || await individualPage.$('a[data-tooltip="Open menu link"]');
+//     let websiteText = website ? await individualPage.evaluate(element => element.getAttribute('href'), website) : '';
+//     console.log(websiteText);
+
+//     const phone = await individualPage.$('button[data-tooltip="Copy phone number"]');
+//     let phoneText = phone ? await individualPage.evaluate(element => element.textContent, phone) : '';
+//     console.log(phoneText);
+
+
+//     await browser.close();
+// })();
